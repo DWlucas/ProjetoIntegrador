c494168be637e13030881c53abe80b4683437284
DWlucas <lucasdwillenbring@gmail.com>
2023-05-13 12:21:41 -0300

    projeto

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let container = document.querySelector('div');
-let input = document.querySelector('#password');
+let input = document.querySelector('password');
 let icon = document.querySelector('.olho');
 
 icon.addEventListener('click', function() {
@@ -14,3 +14,19 @@ icon.addEventListener('click', function() {
 		input.type = 'password';
 	} 
 });
+
+function logar(){
+	var user     = document.getElementById('user').value;
+	var password = document.getElementById('password').value;
+	const userConst = document.querySelector('#user');
+	const passwordConst = document.querySelector('#password');
+
+	if(user == "admin" && password == "admin"){
+		location.href = "home.html";
+	}
+	else{
+		alert('Usuario ou senha incorretos!');
+		userConst.value = '';
+		passwordConst.value = '';
+	}
+}
