84197047f3ee06bfba0494bb4efe8621c04c638a
DWlucas <lucasdwillenbring@gmail.com>
2023-07-06 15:33:21 -0300

    Atualização Banco

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
     session_start();
     unset($_SESSION["usuario"]);
     unset($_SESSION["nome"]);
+    unset($_SESSION["sobrenome"]);
     session_destroy();
     header("Location: index.php");
     exit;
